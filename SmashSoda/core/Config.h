18349cfae513434f394101bb20f38cc1e885d0f2
Mickey Dearman <mickeyuk.dev@gmail.com>
2026-03-30 18:03:46 +0100

    Revert "feat: Add frame pacing."

--- SmashSoda/core/Config.h
+++ SmashSoda/core/Config.h
@@ -61,7 +61,6 @@ public:
 		int windowH = 720;
 		unsigned int resolutionIndex = 0;
 		bool lanczos = true;
-		bool framePacing = true;
 		unsigned int captureMethod = 0;
 		unsigned int fps = 60;
 		unsigned int bandwidth = 20;
